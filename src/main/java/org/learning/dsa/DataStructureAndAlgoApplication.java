ddf774ea7e58c8b4bff8a079d98bcf34ad6aa89d
Gnanendra JN <gnanendra.jn@gmail.com>
2023-05-26 15:05:24 +0530

    First Commit

---
+++ src/main/java/org/learning/dsa/DataStructureAndAlgoApplication.java
@@ -0,0 +1,13 @@
+package org.learning.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAndAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAndAlgoApplication.class, args);
+	}
+
+}
